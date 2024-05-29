b40ead7dbdfb0dcaa245811ac5276d12e055619c
Harsh Jain <harshj2010@gmail.com>
2024-05-29 21:20:57 +0530

    Upading error pages

--- src/Pages/Error.js
+++ src/Pages/Error.js
@@ -9,13 +9,13 @@ export default function Error() {
     }
 
     return (
-        <main className="flex h-screen items-center px-24 justify-between gap-12 font-[raleway]">
-            <img className="w-1/2" src={errorImage} alt="error 404" />
-            <div className="flex flex-col items-start gap-6">
-                <h1 className="text-6xl bg-gradient-to-tl font-bold from-violet-600 to-purple-800 text-transparent bg-clip-text flex items-center gap-3">
+        <main className="flex h-screen items-center px-24 max-sm:px-6 max-sm:py-28 max-sm:flex-col justify-between gap-12 font-[raleway] max-sm:gap-4 max-sm:justify-start">
+            <img className="w-1/2 max-sm:w-full " src={errorImage} alt="error 404" />
+            <div className="flex flex-col items-start gap-6 max-sm:gap-2">
+                <h1 className="text-6xl max-sm:text-4xl bg-gradient-to-tl font-bold from-violet-600 to-purple-800 text-transparent bg-clip-text flex items-center gap-3">
                     Sorry, this page does not exist.
                 </h1>
-                <button className="rounded border border-purple-800 font-normal text-xl text-purple-800 px-4 py-1 transition-all duration-200 hover:text-white hover:shadow hover:bg-purple-700" onClick={handleButtonClick}>Go back</button>
+                <button className="rounded border border-purple-800 font-normal text-xl max-sm:text-lg text-purple-800 px-4 py-1 transition-all duration-200 hover:text-white hover:shadow hover:bg-purple-700" onClick={handleButtonClick}>Go back</button>
             </div>
         </main>
     )
--- src/Pages/UserLoading.js
+++ src/Pages/UserLoading.js
@@ -12,14 +12,26 @@ export default function UserLoading({ username }) {
         <>
             <div className="absolute inset-0 bg-cover backdrop-blur-lg max-sm:w-screen max-sm:h-screen max-sm:overflow-hidden" style={{ backgroundImage: `url(${bgImage})` }}></div>
             <div className="backdrop-blur-3xl w-screen h-screen "></div>
-            <div className='h-screen w-screen flex items-center justify-center absolute top-0'>
+            <div className='h-screen w-screen flex items-center justify-center absolute top-0 sm:hidden'>
                 <div className='w-1/2 flex items-center justify-end'>
-                    <h1 className='animate__animated animate__fadeInLeft  z-50 text-9xl font-semibold user-loading-text'>
+                    <h1 className='animate__animated animate__fadeInDown  z-50 text-9xl max-sm:text-7xl font-semibold user-loading-text'>
                         {firstHalf}
                     </h1>
                 </div>
                 <div className='w-1/2 flex items-center justify-start'>
-                    <h1 className='animate__animated animate__fadeInRight z-50 text-9xl font-semibold user-loading-text'>
+                    <h1 className='animate__animated animate__fadeInUp z-50 text-9xl max-sm:text-7xl font-semibold user-loading-text'>
+                        {secondHalf}
+                    </h1>
+                </div>
+            </div>
+            <div className='h-screen w-screen flex items-center justify-center absolute top-0 max-sm:hidden'>
+                <div className='w-1/2 flex items-center justify-end'>
+                    <h1 className='animate__animated animate__fadeInLeft  z-50 text-9xl max-sm:text-7xl font-semibold user-loading-text'>
+                        {firstHalf}
+                    </h1>
+                </div>
+                <div className='w-1/2 flex items-center justify-start'>
+                    <h1 className='animate__animated animate__fadeInRight z-50 text-9xl max-sm:text-7xl font-semibold user-loading-text'>
                         {secondHalf}
                     </h1>
                 </div>
