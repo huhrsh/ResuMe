b52c47112d70fb094b92fa16387d52190972b315
Harsh Jain <harshj2010@gmail.com>
2024-05-30 15:54:54 +0530

    updating firebase config file

--- src/Firebase.js
+++ src/Firebase.js
@@ -15,8 +15,6 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
-console.log(firebaseConfig)
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
