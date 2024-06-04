9ab418d6b26f88fba4de622afefb8d857c0d1f76
Harsh Jain <harshj2010@gmail.com>
2024-06-04 12:52:56 +0530

    updating reviews and styles

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,10 +8,13 @@ import dashboardImage from "../Assets/Images/Control Panel-bro.png"
 import changeImage from "../Assets/Images/Version control-cuate.png"
 import coffeeDrinkingImage from "../Assets/Images/Coffee break-cuate.png"
 import { useState } from "react"
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/effect-cards';
-import { EffectCards } from 'swiper/modules';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import 'swiper/css';
+// import 'swiper/css/effect-cards';
+// import { EffectCards, EffectCreative } from 'swiper/modules';
+import leftArrow from "../Assets/Images/left-arrow.png"
+import rightArrow from "../Assets/Images/right-arrow.png"
 import Rating from '@mui/material/Rating';
 import graphImage from "../Assets/Images/graph (15).png"
 
@@ -19,8 +22,20 @@ export default function Home() {
     const [reviews, setReviews] = useState([
         { rating: 5, text: "Fantastic website! It made creating and customizing my portfolio site easy and enjoyable. Highly recommend!" },
         { rating: 5, text: "meow meow nyah" },
+        { rating: 5, text: "User friendly and easy to navigate. Would definitely recommend to others for a simple website building experience!" },
         { rating: 5, text: "Excellent tool for creating a custom portfolio website. User-friendly interface and great customization options!" },
     ])
+
+    const [currentSlide, setCurrentSlide] = useState(0);
+
+    const handlePrev = () => {
+        setCurrentSlide((prev) => (prev === 0 ? reviews.length - 1 : prev - 1));
+    };
+
+    const handleNext = () => {
+        setCurrentSlide((prev) => (prev === reviews.length - 1 ? 0 : prev + 1));
+    };
+
 
     return (
         <main className="flex flex-col px-12 max-sm:px-4 font-[raleway] items-center">
@@ -125,12 +140,22 @@ export default function Home() {
             max-sm:text-2xl max-sm:text-center before:sm:hidden
             ">Here's what people say about us</h2>
 
-            <section className="flex px-12 max-sm:px-0 items-center max-sm:flex-col mb-20">
+            {/* <section className="flex px-12 max-sm:px-0 items-center max-sm:flex-col mb-20">
                 <img className="w-6/12 p-6 max-sm:w-full max-sm:p-0" src={coffeeDrinkingImage} alt="dashboard" />
                 <Swiper
-                    effect={'cards'}
                     grabCursor={true}
-                    modules={[EffectCards]}
+                    effect={'creative'}
+                    creativeEffect={{
+                        prev: {
+                            shadow: true,
+                            translate: [0, 0, -400],
+                        },
+                        next: {
+                            translate: ['100%', 0, 0],
+                        },
+                    }}
+                    modules={[EffectCreative]}
+                    className="mySwiper"
                 >
                     {reviews.map((review, index) => (
                         <SwiperSlide
@@ -144,6 +169,37 @@ export default function Home() {
                         </SwiperSlide>
                     ))}
                 </Swiper>
+            </section> */}
+
+            <section className="flex px-12 gap-12 max-sm:px-0 items-center max-sm:flex-col mb-20 justify-between">
+                <img className="w-6/12 p-6 max-sm:w-full max-sm:p-0" src={coffeeDrinkingImage} alt="dashboard" />
+                <div className="relative w-5/12 min-h-72 max-sm:w-5/6 max-sm:min-h-96 p-6 flex flex-col items-center justify-center">
+                    <TransitionGroup>
+                        <CSSTransition
+                            key={currentSlide}
+                            timeout={500}
+                            classNames="fade"
+                        >
+                            <div className="flex flex-col w-full h-full gap-4 absolute left-0 top-0 border border-purple-500 rounded-3xl bg-cover bg-center p-6" >
+                                <Rating defaultValue={reviews[currentSlide].rating} size="large" readOnly className="brightness-110" />
+                                <p className="text-purple-700 font-[raleway] text-xl font-semibold antialiased transition duration-200 h-full">
+                                    {reviews[currentSlide].text}
+                                </p>
+                            </div>
+                        </CSSTransition>
+                    </TransitionGroup>
+                    <button onClick={handlePrev} className="absolute top-1/2 -left-4 transform -translate-y-1/2 text-purple-500 rounded-full bg-white border border-purple-700 p-2 pl-1.5 pr-2.5 max-sm:scale-110" ><img className="h-4 logo" src={leftArrow} alt="left arrow" /></button>
+                    <button onClick={handleNext} className="absolute top-1/2 -right-4 transform -translate-y-1/2 text-purple-500 rounded-full bg-white border border-purple-700 p-2 pr-1.5 pl-2.5 max-sm:scale-110" ><img className="h-4 logo" src={rightArrow} alt="right arrow" /></button>
+                    <div className="flex justify-center mt-4 absolute -bottom-4">
+                        {reviews.map((_, index) => (
+                            <div
+                                key={index}
+                                className={`h-2 w-2 mx-1 rounded-full ${index === currentSlide ? 'bg-purple-700' : 'bg-gray-200'
+                                    }`}
+                            ></div>
+                        ))}
+                    </div>
+                </div>
             </section>
 
         </main>
